88dadfd212ff9cac57befb8088e5b7a5170b0f4d
Artem Naboka <artemnaboka@gmail.com>
2017-07-03 19:35:14 +0300

    Added console shell for class Game. Removed unnecessary files

---
+++ Hamurabi.ConsoleUI/ConsoleGame.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Hamurabi.Core;
+using Hamurabi.Core.Objects.Models;
+
+namespace Hamurabi.ConsoleUI
+{
+    public class ConsoleGame
+    {
+        private int _acrChange;
+        private int _bushelsToFeed;
+        private int _acresToPlant;
+
+        private bool _inputEntered;
+
+        private Game _game;
+
+        public bool IsGameOver { get; private set; }
+
+        public void Start()
+        {
+            _game = new Game();
+            _game.Start();
+            Console.WriteLine("GAME STARTED!");
+            Console.WriteLine(_game.GetInitialReport());
+        }
+
+
+        public void GetInput()
+        {
+            if (_inputEntered)
+            {
+                Console.WriteLine("Input already entered");
+                return;
+            }
+
+            try
+            {
+                Console.WriteLine("How many acres do you wish to buy or sell? (enter a negative amount to sell bushels)");
+                _acrChange = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("How many bushels do you wish to feed your people? (each citizen needs 20 bushels a year)");
+                _bushelsToFeed = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("How many acres do you wish to plant with seed? (each acre takes one bushel)");
+                _acresToPlant = Convert.ToInt32(Console.ReadLine());
+                _inputEntered = true;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Enter valid input (only numbers)");
+            }
+        }
+
+
+        public void MakeTurn()
+        {
+            if (!_inputEntered)
+            {
+                Console.WriteLine("At first make input");
+                return;
+            }
+
+            var playerTurn = new PlayerTurnModel
+            {
+                AcresToPlant = _acresToPlant,
+                AcrChange = _acrChange,
+                BushelsToFeed = _bushelsToFeed
+            };
+
+            var result = _game.MakeTurn(playerTurn);
+            ResetInput();
+
+            if (result.TurnHandleResult == TurnHandleResult.ValidationError)
+            {
+                Console.WriteLine(result.ValidationErrorMessage);
+                return;
+            }
+
+            if (result.TurnHandleResult == TurnHandleResult.GameOver)
+            {
+                IsGameOver = true;
+            }
+
+            Console.WriteLine(result.Report);
+        }
+
+
+        public void ResetInput()
+        {
+            _inputEntered = false;
+        }
+    }
+}
--- Hamurabi.ConsoleUI/Program.cs
+++ Hamurabi.ConsoleUI/Program.cs
@@ -1,49 +1,17 @@
-﻿using System;
-using Hamurabi.Core;
-using Hamurabi.Core.Objects.Models;
-
-namespace Hamurabi.ConsoleUI
+﻿namespace Hamurabi.ConsoleUI
 {
     public class Program
     {
         private static void Main()
         {
-            Game game = new Game();
+            var game = new ConsoleGame();
             game.Start();
-            Console.WriteLine("GAME STARTED!");
-            Console.WriteLine(game.GetInitialReport());
 
-            TurnResultModel result;
-            do
+            while (!game.IsGameOver)
             {
-                Console.WriteLine("How many acres do you wish to buy or sell? (enter a negative amount to sell bushels)");
-                int arcChange = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("How many bushels do you wish to feed your people? (each citizen needs 20 bushels a year)");
-                int bushelsToFeed = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("How many acres do you wish to plant with seed? (each acre takes one bushel)");
-                int acresToPlant = Convert.ToInt32(Console.ReadLine());
-
-                var turnModel = new PlayerTurnModel
-                {
-                    AcrChange = arcChange,
-                    BushelsToFeed = bushelsToFeed,
-                    AcresToPlant = acresToPlant
-                };
-
-                result = game.MakeTurn(turnModel);
-
-                if (result.TurnHandleResult == TurnHandleResult.ValidationError)
-                {
-                    Console.WriteLine(result.ValidationErrorMessage);
-                    continue;
-                }
-
-                Console.WriteLine(result.Report);
-                if (result.TurnHandleResult == TurnHandleResult.GameOver)
-                {
-                    break;
-                }
-            } while (result.TurnHandleResult != TurnHandleResult.GameOver);
+                game.GetInput();
+                game.MakeTurn();
+            }
         }
     }
 }
